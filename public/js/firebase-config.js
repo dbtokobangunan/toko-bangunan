abebf799f4c92b52323452e9b52eeb61b8174cbe
dbtokobangunan <dbtokobangunan@gmail.com>
2025-06-01 18:06:09 +0700

    update firebase config

--- public/js/firebase-config.js
+++ public/js/firebase-config.js
@@ -1,16 +1,21 @@
-// js/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAzzglYwhR2RFJ3hj5DCnHcGBxB9f5fq2U",
-    authDomain: "tokobangunan-997e8.firebaseapp.com",
-    projectId: "tokobangunan-997e8",
-    storageBucket: "tokobangunan-997e8.firebasestorage.app",
-    messagingSenderId: "360315236922",
-    appId: "1:360315236922:web:cd3f5197a1e0c793b67b03",
-    measurementId: "G-Y26BN71EZZ"
+  apiKey: "AIzaSyCtM9BnM3iJF_KcZDHMKE11MOb8EBXDGX0",
+  authDomain: "toko-bangunan01.firebaseapp.com",
+  projectId: "toko-bangunan01",
+  storageBucket: "toko-bangunan01.firebasestorage.app",
+  messagingSenderId: "162547296082",
+  appId: "1:162547296082:web:e455b3d66749fd90003c8b",
+  measurementId: "G-XEXDVSLBGW"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
